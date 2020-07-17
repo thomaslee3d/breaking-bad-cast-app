33240a21e354bef11b9456c9f96a4044bffef02a
thomaslee <thomaslee3d@gmail.com>
2020-07-17 00:04:47 -0400

    recieving data from api

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
-import React from 'react'
-import Header from './components/ui/Header'
-import './App.css'
+import React, { useState, useEffect } from "react"
+import axios from "axios"
+import Header from "./components/ui/Header"
+import "./App.css"
 
 const App = () => {
+  const [items, setItems] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
+
+  useEffect(() => {
+    const fetchItems = async () => {
+      const result = await axios(`https://www.breakingbadapi.com/api/characters`);
+
+      console.log(result.data)
+      setItems(result.data)
+      setIsLoading(false)
+    }
+
+    fetchItems()
+  }, [])
+
   return (
-    <div className='container'>
+    <div className="container">
       <Header />
     </div>
   )
 }
-
-export default App
+export default App;
